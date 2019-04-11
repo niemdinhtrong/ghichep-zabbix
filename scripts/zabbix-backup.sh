a7387b9818efad6006939e008f08f71abc633596
niemdinhtrong <niemdinhtrong@gmail.com>
2019-04-11 16:54:41 +0700

    update

--- scripts/zabbix-backup.sh
+++ scripts/zabbix-backup.sh
@@ -6,6 +6,7 @@ DBHOST="IP-zabbix-server"
 DBNAME="zabbix"
 DBPASSWORD="password"
 DBPORT="3306"
+SAVE_FILE="5"
 #############################################################################################
 PATH_DIR="/root/zabbix"
 Logfile="/root/zabbix/zabbix-backup.log"
@@ -15,4 +16,4 @@ mysqldump -u ${USER_NAME} -h ${DBHOST} -P ${DBPORT} -p${DBPASSWORD} ${DBNAME} ${
 [ "$?" == 0 ] && echo "zabbix-`date +"%Y-%m-%d-%H-%M-%S"`: Backup zabbix succeed"     >> ${Logfile}
 [ "$?" != 0 ] && echo "zabbix-`date +"%Y-%m-%d-%H-%M-%S"`: Backup zabbix not succeed" >> ${Logfile}
 cd ${Backupfile}
-[ "$?" == 0 ] && rm -rf zabbix-$(date +"%Y-%m-%d" --date='5 days ago')*
+[ "$?" == 0 ] && rm -rf zabbix-$(date +"%Y-%m-%d" --date="`$SAVE_FILE` days ago")*
